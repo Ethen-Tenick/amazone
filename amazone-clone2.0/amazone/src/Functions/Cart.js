a7fb799683b9172dde757d1a4f0f17fa9e11dd79
Ethen <ethentenick@gmail.com>
2022-06-24 23:43:14 +0300

    problem displaying after pop is called

--- amazone-clone2.0/amazone/src/Functions/Cart.js
+++ amazone-clone2.0/amazone/src/Functions/Cart.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Cart.css'
+import { v4 } from 'uuid'
 
 function Cart() {
+    const [dataBase, setDataBase] = useState([])
+    const [condition, setCondition] = useState(false)
+
+
     return (
         <div className='cart_main_container'>
             <div>
-                <button>Add to cart</button>
-                <button>View cart</button>
-
+                <button onClick={() => { dataBase.pop() }}>delete from cart</button>
+                <button onClick={() => { setDataBase(prev => [...prev, 455]) }}>Add to cart</button>
+                <button onClick={() => { setCondition(true) }}>View cart {dataBase.length}</button>
             </div>
+            {
+                (condition) ? dataBase.map(item => { return <h4 key={v4()}>{item}</h4> }) : null
+            }
+
         </div>
     )
 }
